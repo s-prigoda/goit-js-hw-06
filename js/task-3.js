961b6bd4521dc0606308559c9234bd02354490b4
Prigoda <sergey.prigoda@gmail.com>
2025-10-06 00:37:28 +0300

    end task-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,34 @@
 'use strict';
+
+class StringBuilder {
+  #value;
+
+  constructor(obj) {
+    this.#value = obj;
+  }
+
+  getValue() {
+    return this.#value;
+  }
+
+  padEnd(str) {
+    this.#value = this.#value + str;
+  }
+
+  padStart(str) {
+    this.#value = str + this.#value;
+  }
+
+  padBoth(str) {
+    this.#value = str + this.#value + str;
+  }
+}
+
+const builder = new StringBuilder('.');
+console.log(builder.getValue()); // "."
+builder.padStart('^');
+console.log(builder.getValue()); // "^."
+builder.padEnd('^');
+console.log(builder.getValue()); // "^.^"
+builder.padBoth('=');
+console.log(builder.getValue()); // "=^.^="
